30ac9ccccf19295d498492727b237837750f1c79
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-10-02 17:40:15 +0300

    Initial commit - Child Immunization System

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDjCdeBC8QFGQJc5-ETda-jtACJT0YGpnY',
+    authDomain: 'cims-61a1c.firebaseapp.com',
+    projectId: 'cims-61a1c',
+    storageBucket: 'cims-61a1c.firebasestorage.app',
+    messagingSenderId: '571895837535',
+    appId: '1:571895837535:web:db512f321fb35fd08afcea',
+    measurementId: 'G-BCSXXP6BY7',
+  },
+};
